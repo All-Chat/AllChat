9b1213402633e113b528ad118ce169d493ef6072
All-Chat <deepak@therealleads.com>
2026-07-17 21:26:16 +0530

    Update layout.tsx

--- app/pricing/layout.tsx
+++ app/pricing/layout.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AllChat – WhatsApp Marketing and Automation Platform",
+  title: "AllChat Pricing – Flexible Plans for Everyone",
   description:
     "Compare AllChat pricing plans and choose the best option for you. From free to advanced features, find a simple, transparent plan that fits your needs.",
 };
