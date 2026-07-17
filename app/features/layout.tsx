87ba7d08791ff5fe5d9f119f4198e82dc598cddd
All-Chat <deepak@therealleads.com>
2026-07-17 21:17:56 +0530

    Create layout.tsx

---
+++ app/features/layout.tsx
@@ -0,0 +1,16 @@
+// app/about/layout.tsx
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "AllChat – WhatsApp Marketing and Automation Platform",
+  description:
+    "Explore AllChat’s key features, including AI-powered chat, automation, integrations, and security options designed to make your daily communication easier.",
+};
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
