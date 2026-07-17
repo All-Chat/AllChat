b3c25446ae515b4f49787ce76a9263ae90020cd5
All-Chat <deepak@therealleads.com>
2026-07-17 21:25:14 +0530

    Update layout.tsx

--- app/acceptable-use/layout.tsx
+++ app/acceptable-use/layout.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AllChat – WhatsApp Marketing and Automation Platform",
+  title: "AllChat Acceptable Use Policy",
   description:
     "Our Acceptable Use Policy to understand the rules for using our AI chat platform safely, responsibly, and in line with legal and ethical standards.",
 };
