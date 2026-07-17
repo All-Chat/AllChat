420765b1399c57a5765244f15b25c41118695424
All-Chat <deepak@therealleads.com>
2026-07-17 21:26:07 +0530

    Update layout.tsx

--- app/features/layout.tsx
+++ app/features/layout.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AllChat – WhatsApp Marketing and Automation Platform",
+  title: "AllChat Features – Smart Tools for Better Conversations",
   description:
     "Explore AllChat’s key features, including AI-powered chat, automation, integrations, and security options designed to make your daily communication easier.",
 };
